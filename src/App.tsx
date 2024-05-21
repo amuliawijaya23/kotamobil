48c3c1975cec3605526f3a3edaa0814b79a16bac
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-21 17:00:43 +0700

    removes mui component from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Box, Paper } from '@mui/material';
 
 function App() {
   return (
     <Router>
-      <main>
-        <Box component={Paper}>
-          <Routes>
-            <Route path="/" element={<>HOME</>} />
-            <Route path="*" element={<>NOT FOUND</>} />
-          </Routes>
-        </Box>
-      </main>
+      <Routes>
+        <Route path="/" element={<>HOME</>} />
+        <Route path="*" element={<>NOT FOUND</>} />
+      </Routes>
     </Router>
   );
 }
