68ac29aa99b47000a0609530318b657bd1561812
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-22 11:01:51 +0700

    resolve vite path

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'node:path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -19,6 +20,9 @@ export default ({ mode }: { mode: string }) => {
           secure: false,
         },
       },
+    },
+    resolve: {
+      alias: [{ find: '~', replacement: resolve(__dirname, './src') }],
     },
   });
 };
