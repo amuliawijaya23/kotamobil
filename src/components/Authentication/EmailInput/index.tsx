e68818879976147e8ce161ef9c503c5a5700c02e
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-22 19:47:00 +0700

    creates email and password input

---
+++ src/components/Authentication/EmailInput/index.tsx
@@ -0,0 +1,38 @@
+import {
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  OutlinedInput,
+} from '@mui/material';
+
+interface EmailInputProps {
+  value: string;
+  isValidEmail: boolean;
+  onChangeHandler: (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => void;
+}
+
+const EmailInput = ({
+  value,
+  isValidEmail,
+  onChangeHandler,
+}: EmailInputProps) => {
+  return (
+    <FormControl size="small" variant="outlined" sx={{ mt: 1 }}>
+      <InputLabel htmlFor="outlined-email">Email</InputLabel>
+      <OutlinedInput
+        type="email"
+        onChange={onChangeHandler}
+        value={value}
+        label="Email"
+        error={!isValidEmail && value.length > 0}
+      />
+      {!isValidEmail && value.length > 0 && (
+        <FormHelperText>Invalid email address</FormHelperText>
+      )}
+    </FormControl>
+  );
+};
+
+export default EmailInput;
